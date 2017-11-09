cc9ba103761472d358a206745d7d6fee2184586d
jinjun.chen@sirun.net <jason-1234>
2017-11-09 16:12:00 +0800

    Initial commit

---
+++ src/main/java/com/fun/HelloFun.java
@@ -0,0 +1,23 @@
+package com.fun;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jason on 2017/11/9
+ * @author jason
+ */
+@RestController
+public class HelloFun {
+
+    @RequestMapping("/getFun")
+    public Map<String,Object> getFun(){
+        Map<String,Object> map=new HashMap<String,Object>();
+        map.put("status","0");
+        map.put("data","hello fun");
+        return map;
+    }
+}

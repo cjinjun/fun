69ff98e2fda7bee9affe769170465ff8724ec67b
jinjun.chen@sirun.net <jason-1234>
2018-01-17 14:50:02 +0800

    update user

--- src/main/java/com/fun/FunApplication.java
+++ src/main/java/com/fun/FunApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FunApplication {
 
+	//测试修改
 	public static void main(String[] args) {
 		SpringApplication.run(FunApplication.class, args);
 	}
